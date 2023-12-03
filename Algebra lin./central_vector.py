9d489d6e1f4474fd37119f90eae80dc4f89b9d52
ilyabeliy <ya.ila.white@gmail.com>
2023-12-03 22:58:19 +0100

    menores and mayores add

--- Algebra lin./central_vector.py
+++ Algebra lin./central_vector.py
@@ -3,9 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class GraphWorker:
-    def __init__(self):
-        ...
-
     @staticmethod
     def draw_graph(g):
         nx.draw(g, with_labels=True, node_size=500, node_color='skyblue')
@@ -20,9 +17,22 @@ class GraphWorker:
         print(text)
         print(self.get_eigenvector_centrality(graph))
 
+    def tres_mas_centrados(self, graf):
+        nodes = list(self.get_eigenvector_centrality(graf).items())
+        return sorted(nodes, reverse=True, key=lambda x: x[1])[:3]
+
+    def tres_menores_centrados(self, graf):
+        nodes = list(self.get_eigenvector_centrality(graf).items())
+        return sorted(nodes, key=lambda x: x[1])[:3]
     def trabajo(self):
         self.example_func(nx.karate_club_graph(), "\nLos resultador para el grafo karate_club_graph: \n")
         self.example_func(nx.florentine_families_graph(), "\nLos resultador para el grafo florentine_families_graph: \n")
+        print("Mas centrados: {} de karate club".format(self.tres_mas_centrados(nx.karate_club_graph())))
+        print("Mas centrados: {} de florentine families".format(self.tres_mas_centrados(nx.florentine_families_graph())))
+        print("Menos centrados de karate club: {} ".format(self.tres_menores_centrados(nx.karate_club_graph())))
+        print("Menos centrados de florentine families: {} ".format(self.tres_menores_centrados(nx.florentine_families_graph())))
+
+
 
 
 if __name__ == "__main__":
